6adac119442d6b5422b526e57333ab47b0d8f3bf
jbhusson <jonah@elixxir.io>
2021-11-18 15:58:37 -0500

    Proper build tags

--- client.go
+++ client.go
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2021 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
+// +build linux darwin windows
+// +build amd64
+
 package main
 
 import "git.xx.network/elixxir/mainnet-commitments/client"
